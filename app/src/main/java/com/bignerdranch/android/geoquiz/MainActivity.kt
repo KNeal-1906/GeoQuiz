e990319c6fd303e567247247b9ea3fc4060c1523
Kenise Neal <kenneal@bu.edu>
2023-10-01 20:17:40 -0400

    adding Geo Quiz App

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.geoquiz.ui.theme.GeoQuizTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        
+    }
+}
